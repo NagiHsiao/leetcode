6f6c29cea5f2f296572352feee6eeb299f46981d
Nagi <nagi.hsiao.co@outlook.com>
2022-07-29 17:22:19 +0800

    593

--- code/593.有效正方形.java
+++ code/593.有效正方形.java
@@ -14,8 +14,3 @@ class Solution {
         return true;
     }
 }
-
-作者：AC_OIer
-链接：https://leetcode.cn/problems/valid-square/solution/by-ac_oier-lwdf/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
